e17a65b7e6e2696b2e68068079d95def7671b4e5
Matthias Andreas Benkard <matthias@benkard.de>
2008-06-12 19:30:43 +0200

    MLKEnvironment: add -initWithBindings, -initWithParent, -addBinding, and -addBindings.

--- MLKEnvironment.h
+++ MLKEnvironment.h
@@ -13,7 +13,11 @@
 
 -(MLKEnvironment *) init;
 -(MLKEnvironment *) initWithParent:(MLKEnvironment *)parent;
+-(MLKEnvironment *) initWithBindings:(NSDictionary *)bindings;
+-(MLKEnvironment *) initWithParent:(MLKEnvironment *)parent bindings:(NSDictionary *)bindings;
 
+-(void) addBindings:(NSDictionary *)bindings;
+-(void) addBinding:(MLKSymbol *)symbol to:(id)value;
 -(void) setBinding:(MLKSymbol *)symbol to:(id)value;
 -(id) valueForBinding:(MLKSymbol *)symbol;
 @end
--- MLKEnvironment.m
+++ MLKEnvironment.m
@@ -17,10 +17,22 @@
   return self;
 }
 
+-(MLKEnvironment *) initWithParent:(MLKEnvironment *)parent bindings:(NSDictionary *)bindings
+{
+  _bindings = [[MLKLinkedList alloc] initWithCons:[parent->_bindings firstCons]];
+  [_bindings push: [NSMutableDictionary dictionaryWithCapacity:10]];
+  [self addBindings: bindings];
+  return self;
+}
+
 -(MLKEnvironment *) initWithParent:(MLKEnvironment *)parent
 {
-  ASSIGN (_bindings, parent->_bindings);
-  return self;
+  return [self initWithParent:parent bindings:nil];
+}
+
+-(MLKEnvironment *) initWithBindings:(NSDictionary *)bindings
+{
+  return [self initWithParent:nil bindings:bindings];
 }
 
 -(void) setBinding:(MLKSymbol *)symbol to:(id)value
@@ -57,5 +69,22 @@
                                           variableName: symbol]
     raise];
   return nil;
+}
+
+-(void) addBindings:(NSDictionary *)bindings
+{
+  int i;
+  NSArray *keys = [bindings allKeys];
+  int count = [keys count];
+  for (i = 0; i < count; i++)
+    {
+      [self addBinding:[keys objectAtIndex:i] to:[bindings objectForKey:[keys objectAtIndex:i]]];
+    }
+}
+
+-(void) addBinding:(MLKSymbol *)symbol to:(id)value
+{
+  NSMutableDictionary *dict = [[_bindings firstCons] car];
+  [dict setObject:value forKey:symbol];
 }
 @end
