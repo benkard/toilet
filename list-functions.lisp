bb2c8f1e2221f7b7d4015fc2cee8df6c9fa6a3e1
Matthias Andreas Benkard <matthias@benkard.de>
2008-07-06 15:14:54 +0200

    Add all missing C[AD]*R functions.

--- list-functions.lisp
+++ list-functions.lisp
@@ -4,24 +4,98 @@
 (%defun first args
   (car (car args)))
 
+
+;;;;-----------------------------------------------------------------
+;;;; THE CxR FUNCTIONS
+;;;;-----------------------------------------------------------------
+(%defun caaaar args
+  (car (caaar (first args))))
+
+(%defun caaadr args
+  (car (caadr (first args))))
+
+(%defun caaar args
+  (car (caar (first args))))
+
+(%defun caadar args
+  (car (cadar (first args))))
+
+(%defun caaddr args
+  (car (caddr (first args))))
+
+(%defun caadr args
+  (car (cadr (first args))))
+
 (%defun caar args
-  (car (cdr (first args))))
+  (car (car (first args))))
 
-(%defun cadr args
-  (car (cdr (first args))))
+(%defun cadaar args
+  (car (cdaar (first args))))
 
-(%defun cdar args
-  (car (cdr (first args))))
+(%defun cadadr args
+  (car (cdadr (first args))))
 
-(%defun cddr args
-  (cdr (cdr (first args))))
+(%defun cadar args
+  (car (cdar (first args))))
+
+(%defun caddar args
+  (car (cddar (first args))))
+
+(%defun cadddr args
+  (car (cdddr (first args))))
 
 (%defun caddr args
   (car (cddr (first args))))
 
+(%defun cadr args
+  (car (cdr (first args))))
+
+(%defun cdaaar args
+  (cdr (caaar (first args))))
+
+(%defun cdaadr args
+  (cdr (caadr (first args))))
+
+(%defun cdaar args
+  (cdr (caar (first args))))
+
+(%defun cdadar args
+  (cdr (cadar (first args))))
+
+(%defun cdaddr args
+  (cdr (caddr (first args))))
+
+(%defun cdadr args
+  (cdr (cadr (first args))))
+
+(%defun cdar args
+  (cdr (car (first args))))
+
+(%defun cddaar args
+  (cdr (cdaar (first args))))
+
+(%defun cddadr args
+  (cdr (cdadr (first args))))
+
+(%defun cddar args
+  (cdr (cdar (first args))))
+
+(%defun cdddar args
+  (cdr (cddar (first args))))
+
+(%defun cddddr args
+  (cdr (cdddr (first args))))
+
 (%defun cdddr args
   (cdr (cddr (first args))))
 
+(%defun cddr args
+  (cdr (cdr (first args))))
+
+
+;;;;-----------------------------------------------------------------
+;;;; SECOND ... TENTH
+;;;;-----------------------------------------------------------------
 (%defun second args
   (cadr (car args)))
 
