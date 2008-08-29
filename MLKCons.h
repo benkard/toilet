f6c59187c654e71d75d97480b96b50d5be6129e6
Matthias Andreas Benkard <matthias@benkard.de>
2008-08-29 16:45:07 +0200

    MLKCons: Improve documentation.

--- MLKCons.h
+++ MLKCons.h
@@ -47,7 +47,7 @@
      car - The car of the new cons cell.
      cdr - The cdr of the new cons cell.
 
-     Returns: A newly allocated cons.
+   Returns: A newly allocated cons.
 */
 +(MLKCons*) cons:(id)car with:(id)cdr;
 
@@ -59,31 +59,50 @@
 
 /* Method: -initWithCar:cdr:
 
-   Initialise a new cons cell with car and cdr.
+   Initialise a new cons cell with a car and cdr.
+
+   Arguments:
+
+     car - The car of the cons cell.
+     cdr - The cdr of the cons cell.
+
+   Returns: The initialised cons cell.
 */
 -(MLKCons*) initWithCar:(id)car cdr:(id)cdr;
 
 /* Method: -car
 
    The car of the cons cell.
+
+   Returns: An object (may be nil).
 */
 -(id) car;
 
 /* Method: -cdr
   
    The cdr of the cons cell.
+
+   Returns: An object (may be nil).
 */
 -(id) cdr;
 
 /* Method: -setCar:
 
    Change the car of the cons cell.
+
+   Arguments:
+
+     value - The new value.
 */
 -(void) setCar:(id)value;
 
 /* Method: -setCdr:
 
    Change the cdr of the cons cell.
+
+   Arguments:
+
+     value - The new value.
 */
 -(void) setCdr:(id)value;
 
