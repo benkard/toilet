a25471b0c682c4e0f04bef536da5c8f248d25c4f
Matthias Andreas Benkard <matthias@benkard.de>
2008-08-29 16:27:36 +0200

    NaturalDocs/ObjC.pm: Add copyright notice.

--- NaturalDocs/ObjC.pm
+++ NaturalDocs/ObjC.pm
@@ -8,6 +8,22 @@
 #
 ###############################################################################
 
+## Copyright 2008, Matthias Andreas Benkard.
+##
+## This package is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This package is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this package; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
 use strict;
 use integer;
 
@@ -25,8 +41,6 @@ sub OnCode
   my $line = $codeLines->[0];
   if ($lastCommentTopicCount)
   {
-    #print "$topic\n";
-    #print "$line\n";
     unless ($topic->Prototype())
     {
       my $code = join ("\n", @{$codeLines});
@@ -52,8 +66,6 @@ sub ParsePrototype
 {
   my ($self, $topic_type, $prototype) = @_;
 
-  #print "$prototype\n";
-
   $_ = $prototype;
   if ($topic_type == ::TOPIC_FUNCTION and /([-+]\s*\((.*?)\)\s*)(.*)/)
   {
@@ -64,7 +76,6 @@ sub ParsePrototype
     $_ = $3;
     while (/(\S+)\((.*?)\)\s*(\S+)(?:(\s+(.*))?|$)/)
     {
-      #print "$4, $1, $2, $3\n";
       $p->AddParameter (NaturalDocs::Languages::Prototype::Parameter->New ("($2)",
                                                                            $1,
                                                                            $3,
